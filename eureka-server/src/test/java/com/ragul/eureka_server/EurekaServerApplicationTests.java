aefc11f6d2940ffd20d490a87f55286533b0717f
iamraguls <raguls4477@gmail.com>
2026-03-14 12:51:08 +0530

    migrated to stable springboot version

---
+++ eureka-server/src/test/java/com/ragul/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragul.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
